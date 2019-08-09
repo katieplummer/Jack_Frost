f9ab2c6a1eb99a0d7cdc1277e65520b927256684
katieplummer <52231969+katieplummer@users.noreply.github.com>
2019-08-09 16:50:55 -0400

    8/9/19-bootstrap front page

---
+++ Jack_Frost/header.php
@@ -0,0 +1,22 @@
+ <!–– Navigation ––> 
+<nav>
+    <a href="https://www.jackfrostgardens.com/"><img class="logo" src="img/JF_logo_2.jpg" alt="HomePage" style= "width: 25%;"></a>
+  <ul>
+  <li class="dropdown">
+     <a href="services/services.html" class="dropbtn">Landscaping</a>
+    <div class="dropdown-content">
+      <a href="#">Consultations</a>
+        <a href="#">Services</a></div></li>
+  <li class="dropdown">
+    <a href="/garden_center.html" class="dropbtn">Garden Center</a>
+    <div class="dropdown-content">
+     <a href="#">Bulk Materials</a>
+      <a href="#">Plants</a>
+        <a href="#">Outdoor Furniture</a></div></li>
+  <li class="dropdown">
+    <a href="about/about.html" class="dropbtn">About Us</a>
+    <div class="dropdown-content">
+        <a href="about/careers.html">Our Story</a></div></li>
+  <li class="dropdown">
+    <a href="contact/contact.html" class="dropbtn">Contact Us</a>
+        </li></ul></nav>
